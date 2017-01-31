aec39e088e8a8f3d274d1d36c33a07652d24a22b
Dimitris Papadiotis <dimitris.papadiotis@gmail.com>
2017-02-01 03:00:32 +1100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,24 +1,49 @@
-## Reading the dataset
-given_data_set <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
-
-## Filtering the original dataset in order to keep what is important to us
+## loading the libraries we need
+library(data.table)
 library(dplyr)
-data_i_caree_about <- filter(given_data_set, Date == "1/2/2007" | Date =="2/2/2007")
+library(magrittr)
+library(lubridate)
 
-## Revoving the original dataset. It is generally good to revome what we dont need anymore (do not be a ragman)
-rm(given_data_set)
+## Setting the working directory
+setwd("D:/portofolio/R Programming/Exploratory Data Analysis")
 
+## Download and unzip the data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./household_power_consumption.zip")
+file <- unzip("./household_power_consumption.zip")
+
+## Reading the data
+data1 <- fread("household_power_consumption.txt", na.strings = "?",
+               colClasses = c("date","time","integer","integer","integer","integer","integer","integer","integer"))
+
+## An alternaive way of reading them
+## data <- read.table(text = grep("^[1,2]/2/2007",readLines(file),value=TRUE), na.strings = '?', sep = ';',
+##                   c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity",
+##                   "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Put the data in a tibble
+data1 <- tbl_df(data1)  %>%    
+  ## Filtering the dataset in order to keep the subset we need
+  filter(Date == "1/2/2007" | Date =="2/2/2007")  %>%
+  ## Merging the columns Date and Time in a single column
+  mutate(date_time = (paste(Date, Time, sep=" "))) %>%
+  ## Getting rid of them
+  select(- Date, - Time)
+## Converting the date_time column from character to POSIX [you can confirm with the function class(data2$date_time)]
+data1$date_time <- dmy_hms(data1$date_time)
+
+## PGN - Plot  ---------------------------------------------------------------------------------------------------------
 ## Opening the png device
 png("plot1.png", width=480, height=480)
 
 ## The code for the first plot
-hist(as.numeric(data_i_caree_about$Global_active_power),     
-                main="Global Active Power", 
-                xlab="Global Active Power (kilowatts)",
-                ylab="Frequency", 
-                col="red")
-                
+hist(data1$Global_active_power,     
+     main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)",
+     ylab="Frequency", 
+     col="red")
+
 ## Closing the png device
-dev.off()  
+dev.off() 
 
 ## THE END
