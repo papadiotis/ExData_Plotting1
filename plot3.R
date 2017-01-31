31098aa7f9aac335292db4198c36fdb2223305b7
Dimitris Papadiotis <dimitris.papadiotis@gmail.com>
2017-02-01 02:51:29 +1100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,36 +1,56 @@
-## Reading the dataset
-given_data_set <- read.table("household_power_consumption.txt", sep = ";", header = TRUE)
-
-## Filtering the original dataset in order to keep what is important to us
+## loading the libraries we need
+library(data.table)
 library(dplyr)
-data_i_caree_about <- filter(given_data_set, Date == "1/2/2007" | Date =="2/2/2007")
+library(magrittr)
+library(lubridate)
 
-## Revoving the original dataset. It is generally good to revome what we dont need anymore (do not be a ragman)
-rm(given_data_set)
+## Setting the working directory
+setwd("D:/portofolio/R Programming/Exploratory Data Analysis")
 
+## Download and unzip the data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./household_power_consumption.zip")
+file <- unzip("./household_power_consumption.zip")
+
+## Reading the data
+data1 <- fread("household_power_consumption.txt", na.strings = "?",
+              colClasses = c("date","time","integer","integer","integer","integer","integer","integer","integer"))
+
+## An alternaive way of reading them
+## data <- read.table(text = grep("^[1,2]/2/2007",readLines(file),value=TRUE), na.strings = '?', sep = ';',
+##                   c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity",
+##                   "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+ 
+## Put the data in a tibble
+data1 <- tbl_df(data1)  %>%    
+## Filtering the dataset in order to keep the subset we need
+filter(Date == "1/2/2007" | Date =="2/2/2007")  %>%
+## Merging the columns Date and Time in a single column
+mutate(date_time = (paste(Date, Time, sep=" "))) %>%
+## Getting rid of them
+select(- Date, - Time)
+  ## Converting the date_time column from character to POSIX [you can confirm with the function class(data2$date_time)]
+  data1$date_time <- dmy_hms(data1$date_time)
+
+## PGN - Plot  ---------------------------------------------------------------------------------------------------------
 ## Opening the png device
 png("plot3.png", width=480, height=480)
 
-## Converting the columns Date and Time
-cbind_Date_Time <- strptime(paste(data_i_caree_about$Date, data_i_caree_about$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-
 ## The code for the third plot
-plot(cbind_Date_Time, as.numeric(data_i_caree_about$Sub_metering_1),
+plot(data1$date_time, data1$Sub_metering_1,
      type = "l",
      main="Global Active Power",
      ylab="Energy Submetering",
      xlab = "")
-lines(cbind_Date_Time, as.numeric(data_i_caree_about$Sub_metering_2),
+lines(data1$date_time, data1$Sub_metering_2,
       type = "l",
       col = "red")
-lines(cbind_Date_Time, as.numeric(data_i_caree_about$Sub_metering_3),
+lines(data1$date_time, data1$Sub_metering_3,
       type = "l",
       col = "blue")
-
-## The top right legend
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lty=1, lwd=1, col=c("black", "red", "blue"))
-               
+
 ## Closing the png device
 dev.off()  
 
