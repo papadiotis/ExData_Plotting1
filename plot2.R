954910e877160a59904eefd9ee110d64d8a2db19
Dimitris Papadiotis <dimitris.papadiotis@gmail.com>
2017-02-01 20:56:40 +1100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,5 @@
 ## loading the libraries we need
+library(pacman)
 pacman::p_load(data.table, dplyr, magrittr, lubridate)
 
 ## Setting the working directory
@@ -13,21 +14,21 @@ file <- unzip("./household_power_consumption.zip")
 data1 <- fread(file, na.strings = "?",
                colClasses = c("date","time","integer","integer","integer","integer","integer","integer","integer"))
 
-## An alternaive way of reading them
+## An alternative way of reading them
 ## data <- read.table(text = grep("^[1,2]/2/2007",readLines(file),value=TRUE), na.strings = '?', sep = ';',
 ##                   c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity",
 ##                   "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Put the data in a tibble
 data1 <- tbl_df(data1)  %>%    
-  ## Filtering the dataset in order to keep the subset we need
-  filter(Date == "1/2/2007" | Date =="2/2/2007")  %>%
-  ## Merging the columns Date and Time in a single column
-  mutate(date_time = (paste(Date, Time, sep=" "))) %>%
-  ## Getting rid of them
-  select(- Date, - Time)
-## Converting the date_time column from character to POSIX [you can confirm with the function class(data2$date_time)]
-data1$date_time <- dmy_hms(data1$date_time)
+## Filtering the dataset in order to keep the subset we need
+filter(Date == "1/2/2007" | Date =="2/2/2007")  %>%
+## Merging the columns Date and Time in a single column
+mutate(date_time = (paste(Date, Time, sep=" "))) %>%
+## Getting rid of them
+select(- Date, - Time)
+  ## Converting the date_time column from character to POSIX [you can confirm with the function class(data2$date_time)]
+  data1$date_time <- dmy_hms(data1$date_time)
 
 ## PGN - Plot  ---------------------------------------------------------------------------------------------------------
 ## Opening the png device
